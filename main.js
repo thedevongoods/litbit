da8120227e576e9c855770671be9ee03ddf8a70a
2dev9 <2dev9@users.noreply.github.com>
2017-02-11 15:07:56 -0500

    Added registration, signup, signoutfunctions to

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+function login{
+	var email = $("#email").val();
+	var password =$("#password").val();
 
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// 
+	});
+
+}
+
+function signin{
+	var email = $("#email").val();
+	var password =$("#password").val();
+	
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		// ...
+	});
+
+}
+
+function signout{
+	firebase.auth().signOut().then(function() {
+  		// Sign-out successful.
+	}, function(error) {
+  		// An error happened.
+	});
+
+}
